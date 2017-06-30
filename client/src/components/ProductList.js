04c0151051367bcd2dc391d8269c8492c4e64b19
Bill Ivan Kooslarto <billivank_96@yahoo.co.id>
2017-07-01 02:22:17 +0700

    added feature sort by name, by pricehightolow, by pricelowtohigh

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { FormGroup, ControlLabel, FormControl, Pagination } from 'react-bootstrap';
+import { Col, FormGroup, ControlLabel, FormControl, Pagination } from 'react-bootstrap';
 
 import ProductCard from './ProductCard';
 
@@ -33,6 +33,28 @@ class ProductList extends Component {
     });
   }
 
+  handleSort(value) {
+    if (value === 'featured'){
+      this.setState({
+        jewelleries: this.state.jewelleries.sort((a,b)=>{
+          if(a.name < b.name) return -1;
+          if(a.name > b.name) return 1;
+          return 0;
+        })
+      })
+    }
+    if (value === 'pricelowtohigh'){
+      this.setState({
+        jewelleries: this.state.jewelleries.sort((a,b)=>Number(a.price.replace(/(^\$|,)/g,''))-Number(b.price.replace(/(^\$|,)/g,'')))
+      })
+    }
+    if (value === 'pricehightolow'){
+      this.setState({
+        jewelleries: this.state.jewelleries.sort((a,b)=>Number(b.price.replace(/(^\$|,)/g,''))-Number(a.price.replace(/(^\$|,)/g,'')))
+      })
+    }
+  }
+
   render() {
     const { jewelleries, currentPage, productPerPage } = this.state;
     const indexOfLastProduct = currentPage * productPerPage;
@@ -44,14 +66,17 @@ class ProductList extends Component {
     }
     return (
       <div style={{ padding: '5%' }}>
-        <FormGroup controlId="formControlsSelect">
-          <ControlLabel>Select</ControlLabel>
-          <FormControl componentClass="select" placeholder="select">
-            <option value="featured">featured</option>
+        <Col xs={12} md={3}>
+        <FormGroup>
+          <ControlLabel>Sort</ControlLabel>
+          <FormControl componentClass="select" onChange={(e)=>{this.handleSort(e.target.value)}}>
+            <option value="featured">Featured</option>
             <option value="pricelowtohigh">Price: Low to High</option>
             <option value="pricehightolow">Price: High to Low</option>
           </FormControl>
         </FormGroup>
+        </Col>
+        <Col xs={12} md={9} style={{textAlign: 'right'}}>
         <Pagination
           prev
           next
@@ -64,6 +89,7 @@ class ProductList extends Component {
           activePage={currentPage}
           onSelect={e=>{ this.handlePage(e) }}
         />
+        </Col>
         <div>
         {currentProducts.map(jewellery =>(
           <ProductCard key={jewellery.productCode} jewellery={jewellery} />
